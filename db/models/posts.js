a0d9dac18b4950f8dca25aa7e1dada29d8ad8946
Shruthi Tuplur <shruthituplur@gmail.com>
2023-09-01 10:19:51 -0700

    all posts adapter functions exist

---
+++ db/models/posts.js
@@ -0,0 +1,43 @@
+
+const client = require('../client');
+
+async function createPost(){
+    return ''
+}
+
+async function getAllPosts(){
+    return ''
+}
+
+async function getPostsByFriends(){
+    return ''
+}
+
+async function getPostsByRestaurant(){
+    return ''
+}
+
+async function getPostById(){
+    return ''
+}
+
+async function updatePost(){
+    return ''
+}
+
+async function deletePost(){
+    return ''
+}
+
+
+
+
+module.exports = {
+    createPost,
+    getAllPosts,
+    getPostsByFriends,
+    getPostsByRestaurant,
+    getPostById,
+    updatePost,
+    deletePost
+}
