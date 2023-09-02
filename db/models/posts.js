916da9bd0a477d9de31c3043fb29f6db70f2bd96
Shruthi Tuplur <shruthituplur@gmail.com>
2023-09-02 11:50:28 -0700

    built createPost db adapter

--- db/models/posts.js
+++ db/models/posts.js
@@ -1,8 +1,20 @@
 
 const client = require('../client');
 
-async function createPost({u}){
-    return ''
+async function createPost({userId, restaurantId, post}){
+    let date = new Date()
+    date = date.toString().split(' ')
+    date = date.slice(1,4).join(' ')
+    
+   try {
+        const {rows : [posts]} = await client.query(`
+        INSERT INTO posts('userId', 'restaurantId', post, date_created)
+        VALUES ($1, $2, $3, $4)
+        RETURNING *;
+        `, [userId, restaurantId, post, date])
+   } catch (error){
+        console.log(error)
+   }
 }
 
 async function getAllPosts(){
