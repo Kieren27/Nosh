5593ea4785540edb87f20c28d3767a1ca8a9c259
Kieren Enright <kierenenright27@gmail.com>
2023-09-06 16:15:33 -0700

    added empty adapters for users.js

---
+++ db/users.js
@@ -0,0 +1,44 @@
+const client = require('../db/client');
+
+async function createUser() {
+
+}
+
+async function getUser() {
+
+}
+
+async function getAllUsers() {
+
+}
+
+async function getUserById() {
+
+}
+
+async function getUserByUsername() {
+
+}
+
+async function getUserByEmail() {
+
+}
+
+async function updateUser() {
+
+}
+
+async function deleteUser() {
+
+}
+
+module.exports = {
+    createUser,
+    getUser,
+    getAllUsers,
+    getUserById,
+    getUserByUsername,
+    getUserByEmail,
+    updateUser,
+    deleteUser
+}
