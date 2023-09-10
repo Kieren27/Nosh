e91600f671dc2fa20738fc788c7eda63d6ad2e3d
HalParadis <halparadis@gmail.com>
2023-09-10 14:36:06 -0700

    added the placeholder adapters and created tests for returned data type

--- db/restaurants.js
+++ db/restaurants.js
@@ -1,9 +1,39 @@
 const client = require('./client');
 
-async function createRestaurant({}) {
+async function createRestaurant() {
+  return 'fake restaurant';
+}
+
+async function getAllRestaurants() {
+  return 'fake restaurants';
+}
+
+async function getRestaurantsByCategory() {
+  return 'fake restaurants';
+}
+
+async function getRestaurantsByName() {
+  return 'fake restaurants';
+}
+
+async function getRestaurantById() {
+  return 'fake restaurant';
+}
+
+async function updateRestaurant() {
+  return 'fake restaurant';
+}
+
+async function deleteRestaurant() {
   return 'fake restaurant';
 }
 
 module.exports = {
-  createRestaurant
+  createRestaurant,
+  getAllRestaurants,
+  getRestaurantsByCategory,
+  getRestaurantsByName,
+  getRestaurantById,
+  updateRestaurant,
+  deleteRestaurant
 }
--- tests/db/restaurants.spec.js
+++ tests/db/restaurants.spec.js
@@ -1,5 +1,13 @@
 require('dotenv').config();
-const { createRestaurant } = require('../../db/restaurants');
+const { 
+  createRestaurant,
+  getAllRestaurants, 
+  getRestaurantsByCategory,
+  getRestaurantsByName,
+  getRestaurantById,
+  updateRestaurant,
+  deleteRestaurant
+} = require('../../db/restaurants');
 const client = require('../../db/client');
 
 describe('Database', () => {
@@ -8,5 +16,70 @@ describe('Database', () => {
   })
   afterAll(async() => {
     client.end();
+  })
+
+  describe('createRestaurant', () => {
+    it('returns an object', async () => {
+      const res = await createRestaurant({
+        name: 'Pizza Hut',
+        cuisine: 'American',
+        price: 1,
+        address: '123 Smith St'
+      });
+      expect(typeof res === 'object' &&
+        !Array.isArray(res) &&
+        res !== null).toBe(true);
+    })
+  })
+
+  describe('getAllRestaurants', () => {
+    it('returns an array', async () => {
+      const allRes = await getAllRestaurants();
+      expect(Array.isArray(allRes)).toBe(true);
+    })
+  })
+
+  describe('getRestaurantsByCategory', () => {
+    it('returns an array', async () => {
+      const resByCat = await getRestaurantsByCategory('American');
+      expect(Array.isArray(resByCat)).toBe(true);
+    })
+  })
+
+  describe('getRestaurantsByName', () => {
+    it('returns an array', async () => {
+      const resByName = await getRestaurantsByName('Pizza Hut');
+      expect(Array.isArray(resByName)).toBe(true);
+    })
+  })
+
+  describe('getRestaurantById', () => {
+    it('returns an object', async () => {
+      const resById = await getRestaurantById(1);
+      expect(typeof resById === 'object' &&
+        !Array.isArray(resById) &&
+        resById !== null).toBe(true);
+    })
+  })
+
+  describe('updateRestaurant', () => {
+    it('returns an object', async () => {
+      const updatedRes = await updateRestaurant({
+        id: 1,
+        name: 'The Pizza Hut'
+      });
+      expect(typeof updatedRes === 'object' &&
+        !Array.isArray(updatedRes) &&
+        updatedRes !== null).toBe(true);
+    })
+  })
+
+  describe('deleteRestaurant', () => {
+    it('returns an object', async () => {
+      const deletedRes = await deleteRestaurant(1);
+      expect(typeof deletedRes === 'object' &&
+        !Array.isArray(deletedRes) &&
+        deletedRes !== null).toBe(true);
+    })
   })
 })
