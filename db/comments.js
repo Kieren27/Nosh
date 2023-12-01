f7e28cdb4022a28c60462e5e2387633dc1bb4adf
Kieren Enright <kierenenright27@gmail.com>
2023-12-01 13:05:13 -0800

    added new file 'comments.js' with boilerplate functions

---
+++ db/comments.js
@@ -0,0 +1,34 @@
+const client = require('./client');
+
+async function createComment() {
+    return 'fake comment';
+}
+
+async function getCommentByUser() {
+    return 'fake comment';
+}
+
+async function getCommentByPost() {
+    return 'fake comment';
+}
+
+async function getCommentById() {
+    return 'fake comment';
+}
+
+async function updateComment() {
+    return 'fake comment';
+}
+
+async function deleteComment() {
+    return 'fake comment';
+}
+
+module.exports = {
+    createComment,
+    getCommentByUser,
+    getCommentByPost,
+    getCommentById,
+    updateComment,
+    deleteComment
+}
