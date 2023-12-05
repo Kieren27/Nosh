4fc1eed4e82bb1b72fa0c78972003573e2dad17b
Kieren Enright <kierenenright27@gmail.com>
2023-12-04 19:50:00 -0800

    created new files: posts.js (with boilerplate) + associated posts.spec.js

---
+++ db/posts.js
@@ -0,0 +1,39 @@
+const client = require('./client');
+
+async function createPost() {
+    return 'fake post';
+}
+
+async function getAllPosts() {
+    return 'fake post';
+}
+
+async function getPostsByUser() {
+    return 'fake post';
+}
+
+async function getPostsByFriends() {
+    return 'fake post';
+}
+
+async function getPostById() {
+    return 'fake post';
+}
+
+async function updatePost() {
+    return 'fake post';
+}
+
+async function deletePost() {
+    return 'fake post';
+}
+
+module.exports = {
+    createPost,
+    getAllPosts,
+    getPostsByUser,
+    getPostsByFriends,
+    getPostById,
+    updatePost,
+    deletePost
+}
